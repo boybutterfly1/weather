8754137a6794e2ac9a065dcdcec0d83e34eda192
Kamil Mukhtarov <boybutterfly1@MacBook-Pro-Kamil.local>
2023-11-13 21:55:59 +0300

    weather request changed to https

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
 export const WEATHER_API_KEY = '469974aa0ccb4bb2818102000231111'
-export const BASE_URL = 'http://api.weatherapi.com/v1'
+export const BASE_URL = 'https://api.weatherapi.com/v1'
 
 export const YMAP_API_KEY = '261e2dd6-a9bb-498a-860f-747e736c931a'
